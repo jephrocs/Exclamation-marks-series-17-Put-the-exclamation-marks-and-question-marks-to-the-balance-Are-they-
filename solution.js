615232d420f313d97ebcd4a54794cc48daa9d990
Jeff C <50182734+jephrocs@users.noreply.github.com>
2020-09-01 17:13:21 -0700

    added solution

---
+++ solution.js
@@ -0,0 +1,30 @@
+balance = (left, right) => {
+    var lleft = 0;
+    var rright = 0;
+    for (var i = 0; i < left.length; i++) {
+        if (left[i] === '!') {
+            lleft++
+            lleft++
+        } else if (left[i] === '?') {
+            lleft++
+            lleft++
+            lleft++
+        }
+    }
+    for (var j = 0; j < right.length; j++) {
+        if (right[j] === '!') {
+            rright++
+            rright++
+        } else if (right[j] === '?') {
+            rright++
+            rright++
+            rright++
+        }
+    }
+    if (lleft > rright) {
+        return "Left"
+    } if (lleft < rright) {
+        return "Right"
+    } else return "Balance"
+
+}
